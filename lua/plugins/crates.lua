64813100f36c4f9c35c28708656a0971a6668164
sigpher <425223341@qq.com>
2023-06-05 17:06:29 +0800

    add some plugins

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -1,11 +1,21 @@
 return {
-    {
-        "saecki/crates.nvim",
-        ft = {"rust","toml"},
-        config = function (_,opts)
-            local crates = require("crates")
-            crates.setup(opts)
-            crates.show()
-        end
-    }
+  {
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+      crates.open_homepage()
+      crates.open_repository()
+      crates.open_crates_io()
+      crates.open_documentation()
+      crates.popup_available()
+      crates.show_popup()
+      crates.show_crate_popup()
+      crates.show_versions_popup()
+      crates.show_features_popup()
+      crates.show_dependencies_popup()
+    end,
+  },
 }
