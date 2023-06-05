25cef6c0ef3352d2da8f17fbd67cb2f7b785f6d4
sigpher <425223341@qq.com>
2023-06-06 07:29:01 +0800

    delete something doesn't work

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -6,16 +6,6 @@ return {
       local crates = require("crates")
       crates.setup(opts)
       crates.show()
-      crates.open_homepage()
-      crates.open_repository()
-      crates.open_crates_io()
-      crates.open_documentation()
-      crates.popup_available()
-      crates.show_popup()
-      crates.show_crate_popup()
-      crates.show_versions_popup()
-      crates.show_features_popup()
-      crates.show_dependencies_popup()
     end,
   },
 }
